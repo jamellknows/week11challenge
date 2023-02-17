5dc037cd5cad9c5d724e51d7e49e1877604978de
jamellknows <jamellsamuels@googlemail.com>
2023-02-17 21:21:59 +0000

    Set up questions scaffold

--- index.js
+++ index.js
@@ -10,14 +10,55 @@ inquirer.prompt([
         type: 'input',
         message: 'Title',
         name: 'title'
-    }
+    },
     {
         type: 'input', 
         message: 'Description',
         name: 'description'
-    }
+    },
+    {
+        type: 'confirm', 
+        message: 'Table of Contents',
+        name: 'tableOfContents',
+    },
+    {
+        type: 'raw-list',
+        message: 'Enter the sections of the contents',
+        name: 'list-of-contents',
+        when: (answers) => answers.tableOfContents === true
+    },
+    {
+        type: 'input', 
+        message: 'Installation',
+        name: 'installation'
+    },
+    {
+        type: 'input', 
+        message: 'Usage',
+        name: 'usage'
+    },
+    {
+        type: 'input', 
+        message: 'License',
+        name: 'license'
+    },
+    {
+        type: 'input', 
+        message: 'Contributing',
+        name: 'contributing'
+    },
+    {
+        type: 'input', 
+        message: 'Tests',
+        name: 'tests'
+    },
+    {
+        type: 'input', 
+        message: 'Questions',
+        name: 'questions'
+    },
    
-])
+]).then((response) => console.log(response))
 const questions = [
 
 ];
