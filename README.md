51b035528d3a3376fd35bb1ed596df68b11b9536
jamellknows <jamellsamuels@googlemail.com>
2023-02-18 04:02:27 +0000

    Updated README using the script

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Week 11 Challenge - README Generator 
+  ---
+  ## Description
+  This is the module 11 challenege project which is a readme generator, the readme for this project was generated using the contained script. 
+  ---
+  ## Table of Contents
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license) 
+  null
+  [Tests](#tests) 
+  [Questions](#questions) 
+  
+  ---
+  ## Installation 
+  To install go to the file directory where index.js is located and run node index.js if you do not have the packages installed first install node by going to https://nodejs.org/en/ . If you do not have inquirer installed install it by running npm install -- save inquirer
+  ---
+  ## Usage 
+  The intended use for this product is to generate README files. 
+  ---
+  ## License 
+  The MIT License (MIT)
+  =====================
+  
+  Copyright © 2023 jamellknows
+  
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the “Software”), to deal in the Software without
+  restriction, including without limitation the rights to use,
+  copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following
+  conditions:
+  
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+  ---
+  ## Contributing 
+  If you wish to contribute to this project please fork it from this repository and then request for the changes to be merged. If they are found to be acceptable they will bemerged. Otherwise contact me (jamellknows) jamellsamuels@googlemail.com
+  ---
+  ## Tests 
+  This product was tested by creating a series of sample README's and this README
+  ---
+  ## Questions 
+  jamellknows
+  jamellsamuels@googlemail.com
+  ---
 
--- index.js
+++ index.js
@@ -4,90 +4,90 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-
-inquirer.prompt([
-    {
-        type: 'input',
-        message: 'Title',
-        name: 'title'
-    },
-    {
-        type: 'input', 
-        message: 'Description',
-        name: 'description'
-    },
-    {
-        type: 'confirm', 
-        message: 'Table of Contents',
-        name: 'tableOfContents',
-    },
-    {
-        type: 'input', 
-        message: 'Installation Instructions',
-        name: 'installation'
-    },
-    {
-        type: 'input', 
-        message: 'Usage Information',
-        name: 'usage'
-    },
-    {
-        type: 'confirm', 
-        message: 'License',
-        name: 'license'
-    },
-    {
-        type: 'list',
-        message: 'Please select the type of license you would like to apply?',
-        name: 'licenseType',
-        choices: ['Apache 2.0', 'Berkeley Software Distribution OS BSD', 'GNU licenses', 'MIT License', 'Mozilla Public License 2.0'],
-        when: (answers) => answers.license === true
-    },
-    {
-        type: 'confirm',
-        message: 'Do you have any contributors?',
-        name: 'contributorsBoolean',
-    },
-    {
-        type: 'input', 
-        message: 'Names of Contributors',
-        name: 'contributions', 
-        when: (answers) => answers.contributorsBoolean === true
-    },
-    {
-        type: 'input',
-        message: 'contribution guidelines',
-        name: 'contributionGuidelines'
-    },
-    {
-        type: 'input', 
-        message: 'Tests',
-        name: 'tests'
-    },
-    {
-        type: 'input', 
-        message: 'For questions, please enter your username',
-        name: 'questionsUsername'
-    },
-    {
-        type: 'input', 
-        message: 'For questions, please enter your email address',
-        name: 'questionsEmail'
-    },
-   
-]).then((response) => generateMarkdown(response))
-    .then((markdown) => fs.writeFileSync('./output/README.md', markdown))
-    .catch((err) => console.log(err))
-const questions = [
-
-];
+function getPrompt(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Title',
+            name: 'title'
+        },
+        {
+            type: 'input', 
+            message: 'Description',
+            name: 'description'
+        },
+        {
+            type: 'confirm', 
+            message: 'Table of Contents',
+            name: 'tableOfContents',
+        },
+        {
+            type: 'input', 
+            message: 'Installation Instructions',
+            name: 'installation'
+        },
+        {
+            type: 'input', 
+            message: 'Usage Information',
+            name: 'usage'
+        },
+        {
+            type: 'confirm', 
+            message: 'License',
+            name: 'license'
+        },
+        {
+            type: 'list',
+            message: 'Please select the type of license you would like to apply?',
+            name: 'licenseType',
+            choices: ['Apache 2.0', 'Berkeley Software Distribution OS BSD', 'GNU licenses', 'MIT License', 'Mozilla Public License 2.0'],
+            when: (answers) => answers.license === true
+        },
+        {
+            type: 'confirm',
+            message: 'Do you have any contributors?',
+            name: 'contributorsBoolean',
+        },
+        {
+            type: 'input', 
+            message: 'Names of Contributors',
+            name: 'contributions', 
+            when: (answers) => answers.contributorsBoolean === true
+        },
+        {
+            type: 'input',
+            message: 'contribution guidelines',
+            name: 'contributionGuidelines'
+        },
+        {
+            type: 'input', 
+            message: 'Tests',
+            name: 'tests'
+        },
+        {
+            type: 'input', 
+            message: 'For questions, please enter your username',
+            name: 'questionsUsername'
+        },
+        {
+            type: 'input', 
+            message: 'For questions, please enter your email address',
+            name: 'questionsEmail'
+        },
+       
+    ]).then((response) => generateMarkdown(response))
+        .then((markdown) => writeToFile("README.md", markdown))
+        .catch((err) => console.log(err))
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFileSync(`./output/${fileName}`,data )
 }
 
 // function to initialize program
 function init() {
+    getPrompt()
 
 }
 
